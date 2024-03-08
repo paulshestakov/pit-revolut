d3e885ec50f733f7664e5c4810445f64a4d73910
Paul Shestakov <paulshestakov@icloud.com>
2024-03-08 01:47:16 +0100

    Stock profit tests

--- src/profits.test.ts
+++ src/profits.test.ts
@@ -2,7 +2,7 @@ import { expect, test, describe } from "bun:test";
 import type { Exchange, ExchangeCurrency } from "./exchange";
 import { makeProfits } from "./profits";
 import { range } from "./array";
-import { TransactionType, type DividendTransaction } from "./revolut";
+import { TransactionType, type DividendTransaction, type SellTransaction, type BuyTransaction } from "./revolut";
 
 const mockExchange: Exchange = {
   getRate: async (currency: ExchangeCurrency, date: Date) => {
@@ -22,5 +22,59 @@ describe("profits", () => {
       "FX Rate": "0.2445",
     }));
     expect(await profits.getProfits(transactions)).toEqual({ dividends: 20.4, stock: 0 });
+  });
+
+  test("stocks positive", async () => {
+    const profits = makeProfits(mockExchange);
+    const transactions: (SellTransaction | BuyTransaction)[] = [
+      {
+        Type: TransactionType.BUY_MARKET,
+        Ticker: "AMZN",
+        Quantity: 31,
+        "Price per share": "$139.54",
+        Date: "2023-09-18T17:40:15.268Z",
+        "Total Amount": "$4347.44",
+        Currency: "USD",
+        "FX Rate": "0.2309",
+      },
+      {
+        Type: TransactionType.SELL_MARKET,
+        Ticker: "AMZN",
+        Quantity: 31,
+        "Price per share": "$141.28",
+        Date: "2023-11-08T17:57:50.047Z",
+        "Total Amount": "$4,368.69",
+        Currency: "USD",
+        "FX Rate": "0.2419",
+      },
+    ];
+    expect(await profits.getProfits(transactions)).toEqual({ dividends: 0, stock: 21.25 });
+  });
+
+  test("stocks negative", async () => {
+    const profits = makeProfits(mockExchange);
+    const transactions: (SellTransaction | BuyTransaction)[] = [
+      {
+        Type: TransactionType.SELL_MARKET,
+        Ticker: "AMZN",
+        Quantity: 31,
+        "Price per share": "$139.54",
+        Date: "2023-09-18T17:40:15.268Z",
+        "Total Amount": "$4347.44",
+        Currency: "USD",
+        "FX Rate": "0.2309",
+      },
+      {
+        Type: TransactionType.BUY_MARKET,
+        Ticker: "AMZN",
+        Quantity: 31,
+        "Price per share": "$141.28",
+        Date: "2023-11-08T17:57:50.047Z",
+        "Total Amount": "$4,368.69",
+        Currency: "USD",
+        "FX Rate": "0.2419",
+      },
+    ];
+    expect(await profits.getProfits(transactions)).toEqual({ dividends: 0, stock: -21.25 });
   });
 });
